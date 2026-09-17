99cbb1210e6d67140c030c0d12e5c9c270ab169b
Elisabet <Dasaia070920@gmail.com>
2026-09-18 00:30:25 +0200

    Corregir página de confirmación de pedido

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -238,7 +238,7 @@ export async function POST(request: Request) {
       },
 
       success_url:
-        `${baseUrl}/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
+        `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
 
       cancel_url:
         `${baseUrl}/checkout/cancel`,
